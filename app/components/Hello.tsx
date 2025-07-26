a938891745adb328c20433cec3faef6e2c336388
Jatin Sharma <jatina9410@gmail.com>
2025-07-26 22:06:29 +0530

    setup

---
+++ app/components/Hello.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+console.log('i am a serrver componnet');
+
+const Hello = () => {
+    console.log('i m a client component');
+    
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Hello
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Hello from "./components/Hello";
+
+export default function Home() {
+  return (<>
+  <Hello/>
+   <h1 className="bg-red-900 text-4xl">hey ima learning nect js</h1>
+  </>
+  );
+}
